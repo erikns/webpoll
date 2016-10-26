29a38dcef7029918d7ac380cd906932c2229a044
h117408 <h117408@9fc1927c-0711-df4b-8656-0a05da22bc1c>
2016-10-26 10:58:12 +0000

    La til avtalte metoder og attributter i QuestionModel

--- src/main/java/no/hib/megagruppe/webpoll/models/QuestionModel.java
+++ src/main/java/no/hib/megagruppe/webpoll/models/QuestionModel.java
@@ -1,4 +1,30 @@
 package no.hib.megagruppe.webpoll.models;
 
 public class QuestionModel {
+    private String[] options;
+    private String text;
+    private String answer[];
+
+
+    public void submitAnswer(String s){
+
+    }
+
+    public void submitAnswer(String[] s){
+
+    }
+
+    //public Enum getType(){
+    //    return Enum;
+    //}
+
+    /**
+     *
+     * @return
+     */
+    public String[] getOptions(){
+        return options;
+    }
+
+
 }
