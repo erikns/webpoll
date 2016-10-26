272af2308d0af42fe6cf2b88cebf116152464bf1
Erik <erikns@users.noreply.github.com>
2016-10-26 14:07:23 +0000

    Add some test data to InMemorySurveyRepository awaiting the database

--- src/main/java/no/hib/megagruppe/webpoll/data/InMemorySurveyRepository.java
+++ src/main/java/no/hib/megagruppe/webpoll/data/InMemorySurveyRepository.java
@@ -1,5 +1,7 @@
 package no.hib.megagruppe.webpoll.data;
 
+import no.hib.megagruppe.webpoll.entities.OptionEntity;
+import no.hib.megagruppe.webpoll.entities.QuestionEntity;
 import no.hib.megagruppe.webpoll.entities.SurveyEntity;
 import no.hib.megagruppe.webpoll.entities.UserEntity;
 
@@ -18,14 +20,54 @@ public class InMemorySurveyRepository implements SurveyRepository {
         user.setLastName("Testesen");
         user.setId(1);
 
-        SurveyEntity entity = new SurveyEntity();
-        entity.setId(1);
-        entity.setName("Testundersøkelse");
-        entity.setDate(new Date(System.currentTimeMillis() - 3600));
-        entity.setDeadline(new Date(System.currentTimeMillis() + 36000));
-        entity.setOwner(user);
+        //////
+        OptionEntity optionA = new OptionEntity();
+        optionA.setId(1);
+        optionA.setText("Ja");
 
-        surveys.put(entity.getId(), entity);
+        OptionEntity optionB = new OptionEntity();
+        optionB.setId(2);
+        optionB.setText("Nei");
+
+        QuestionEntity question1 = new QuestionEntity();
+        question1.setId(1);
+        question1.setText("Har du noen gang programmert JavaEE?");
+        question1.setMultiple(false);
+        question1.setType(QuestionEntity.QuestionType.MULTIPLE_CHOICE);
+
+        List<OptionEntity> options = new ArrayList<>();
+        options.add(optionA);
+        options.add(optionB);
+        question1.setOptions(options);
+
+        optionA.setQuestion(question1);
+        optionB.setQuestion(question1);
+        //////
+        //////
+        QuestionEntity question2 = new QuestionEntity();
+        question2.setId(2);
+        question2.setText("Hva synes du om WebPoll?");
+        question2.setType(QuestionEntity.QuestionType.FREE_TEXT);
+        //////
+
+        SurveyEntity survey = new SurveyEntity();
+        survey.setId(1);
+        survey.setName("Testundersøkelse");
+        survey.setDate(new Date(System.currentTimeMillis() - 3600));
+        survey.setDeadline(new Date(System.currentTimeMillis() + 36000));
+        survey.setOwner(user);
+        survey.setActive(true);
+        survey.setCode("testabc");
+
+        List<QuestionEntity> questions = new ArrayList<>();
+        question1.setSurvey(survey);
+        question2.setSurvey(survey);
+        questions.add(question1);
+        questions.add(question2);
+
+        survey.setQuestions(questions);
+
+        surveys.put(survey.getId(), survey);
     }
 
     @Override
