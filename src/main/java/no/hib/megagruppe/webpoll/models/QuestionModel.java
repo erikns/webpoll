c61d6bc437f006c30f1ccf893a14770a838a4499
h117408 <h117408@9fc1927c-0711-df4b-8656-0a05da22bc1c>
2016-10-26 11:07:43 +0000

    La til javadoc i QuestionModel

--- src/main/java/no/hib/megagruppe/webpoll/models/QuestionModel.java
+++ src/main/java/no/hib/megagruppe/webpoll/models/QuestionModel.java
@@ -6,21 +6,35 @@ public class QuestionModel {
     private String answer[];
 
 
+    /**
+     * Method to submit a single answer to an Open- or Mulitple-ChoiceQuestion
+     * Used if multiple answers are not allowed
+     * @param s
+     */
     public void submitAnswer(String s){
-
+        //FIXME
     }
-
-    public void submitAnswer(String[] s){
-
-    }
-
-    //public Enum getType(){
-    //    return Enum;
-    //}
 
     /**
-     *
-     * @return
+     * Method to submit more than one answer if the question allows it
+     * @param s an array of Strings, each representing an answer
+     */
+    public void submitAnswer(String[] s){
+        //FIXME
+    }
+
+    /**
+     * @return enum question type
+     */
+    public Enum getType(){
+        //FIXME need to implement this Enum, -OpenQuestion-, -MultipleChoiceQuestion- and add it to this method
+        return null;
+    }
+
+    /**
+     * Method that returns the options from a question, if question 'text'
+     * this method will return an empty array
+     * @return an array of strings for each option in the question
      */
     public String[] getOptions(){
         return options;
