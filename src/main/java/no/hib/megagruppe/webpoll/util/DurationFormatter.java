cd1ad8158dad5d74aa1c6444be845c6f1327036d
h117408 <h117408@9fc1927c-0711-df4b-8656-0a05da22bc1c>
2016-10-28 13:31:05 +0000

    cleaned up DurationFormatter class

--- src/main/java/no/hib/megagruppe/webpoll/util/DurationFormatter.java
+++ src/main/java/no/hib/megagruppe/webpoll/util/DurationFormatter.java
@@ -2,7 +2,14 @@ package no.hib.megagruppe.webpoll.util;
 
 import java.time.Duration;
 
+import static jdk.nashorn.internal.objects.NativeDate.getSeconds;
+
 public class DurationFormatter {
+
+	final static long DAY_CONVERTION = 86400;
+	final static long HOUER_CONVERTION = 3600;
+	final static long MINUTE_CONVERSION = 3600;
+	final static long SECOND_CONVERSION = 60;
 
 	/**
 	 * Formatterer en duration til en String med format 0t, 00m, 39s.
@@ -11,13 +18,35 @@ public class DurationFormatter {
 	 */
 	public static String formatDuration(Duration duration) {
 	    long seconds = duration.getSeconds();
-	    long absSeconds = Math.abs(seconds);
-	    String positive = String.format(
+
+		String positive = String.format(
 	        "%dd, %dt, %02dm, %02ds",
-	        absSeconds / 86400,
-	        (absSeconds % 24) / 3600,
-	        (absSeconds % 3600) / 60,
-	        absSeconds % 60);
+	        getDays(duration),
+	        getHouers(duration),
+	        getMinutes(duration),
+	        getSecondsF(duration));
 	    return seconds < 0 ? "-" + positive : positive;
 	}
+
+
+	/**
+	 * Getters for formatDuration()
+	 */
+	private static long getDays(Duration duration){
+		Long totalSecs = duration.getSeconds();
+		return totalSecs/DAY_CONVERTION;
+	}
+	private static long getHouers(Duration duration){
+		Long totalSecs = duration.getSeconds();
+		return (totalSecs % DAY_CONVERTION) / HOUER_CONVERTION;
+	}
+	private static long getMinutes(Duration duration){
+		Long totalSecs = duration.getSeconds();
+		return (totalSecs % HOUER_CONVERTION) / SECOND_CONVERSION;
+	}
+	private static long getSecondsF(Duration duration){
+		Long totalSecs = duration.getSeconds();
+		return (totalSecs % SECOND_CONVERSION);
+	}
+
 }
