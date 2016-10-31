4d111847799d56ee469bf20e7b0047b0076df9be
Erik <erikns@users.noreply.github.com>
2016-10-31 10:22:27 +0000

    Finish logout user story

--- src/main/java/no/hib/megagruppe/webpoll/servlets/lecturer/LogoutServlet.java
+++ src/main/java/no/hib/megagruppe/webpoll/servlets/lecturer/LogoutServlet.java
@@ -1,35 +1,30 @@
 package no.hib.megagruppe.webpoll.servlets.lecturer;
 
+import no.hib.megagruppe.webpoll.services.SecurityService;
+
 import java.io.IOException;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LogoutServlet
- */
+
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
+    @EJB
+    SecurityService securityService;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// TODO
-		request.getRequestDispatcher("/WEB-INF/lecturer/signedout.jsp").forward(request, response);;
-		
+		if (securityService.isLoggedIn()) {
+            securityService.logOut();
+            request.getRequestDispatcher("WEB-INF/lecturer/signedout.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(getServletContext().getContextPath() + "/");
+        }
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
 }
