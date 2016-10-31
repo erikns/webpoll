208a5affb510c9c52818b40c0d182f2be6f9f6bd
Erik <erikns@users.noreply.github.com>
2016-10-31 11:14:20 +0000

    Update javadoc in SurveyLecturerService

--- src/main/java/no/hib/megagruppe/webpoll/services/SurveyLecturerService.java
+++ src/main/java/no/hib/megagruppe/webpoll/services/SurveyLecturerService.java
@@ -5,7 +5,9 @@ import no.hib.megagruppe.webpoll.models.SurveyOverviewModel;
 import java.util.List;
 
 /**
- * Interface to support the use cases of the lecturer editing / viewing surveys
+ * Interface to support the use cases of the lecturer editing / viewing surveys.
+ * Note: this service assumes nothing about security, and expects the checking of
+ * user credentials etc to be done before any service method is called.
  */
 public interface SurveyLecturerService {
     // NOTES:
