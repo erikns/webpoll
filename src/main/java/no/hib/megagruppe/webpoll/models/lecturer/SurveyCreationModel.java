63872ba8b11663ff83a192e113b0dc154374f9d7
h148907 <magnusdk@users.noreply.github.com>
2016-11-01 11:07:42 +0000

    Implementert de viktigeste metodene i surveyCreationModel og questionCreationModel

--- src/main/java/no/hib/megagruppe/webpoll/models/lecturer/SurveyCreationModel.java
+++ src/main/java/no/hib/megagruppe/webpoll/models/lecturer/SurveyCreationModel.java
@@ -1,13 +1,133 @@
 package no.hib.megagruppe.webpoll.models.lecturer;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import no.hib.megagruppe.webpoll.entities.QuestionEntity;
+import no.hib.megagruppe.webpoll.entities.SurveyEntity;
+import no.hib.megagruppe.webpoll.entities.UserEntity;
+
 public class SurveyCreationModel {
 
 	/* Funksjoner:
 	 *	- Gi navn
 	 * 	- Nytt spørsmål
 	 * 	- Fjern et spørsmål fra listen.
-	 * 	- Lagre (Dersom har minst 1 spørsmål)
+	 * 	- Lagre til database (Dersom har minst 1 spørsmål)
 	 * 
 	 */
+
+	private SurveyEntity survey;
+	
+	private String name;
+	private UserEntity owner;
+	
+	private List<SurveyQuestionCreationModel> questions;
+	
+	public SurveyCreationModel(UserEntity owner){
+		survey = new SurveyEntity();
+		survey.setOwner(owner);
+	}
+	
+	
+	
+	
+	
+
+	
+	/**
+	 * Adds a question to the survey.
+	 * @param question The question to be added.
+	 */
+	public void addQuestionCreationModel(SurveyQuestionCreationModel question){
+		questions.add(question);
+	}
+	
+	/**
+	 * Removes a question from the survey in the specified position.
+	 * @param question The index of the question to be removed.
+	 */
+	public void removeQuestionCreationModel(int questionNumber){
+		questions.remove(questionNumber);
+	}
+	
+	
+	
+	/**
+	 * Checks if the survey is ready to be built.
+	 * The name must not be empty,
+	 * the owner must exist,
+	 * and there must be atleast one question.
+	 * @return True if name's not empty, owner's not null, and has atleast one question.
+	 */
+	public boolean isReady(){
+		boolean nameReady = name != null && name != ""; // Name can't be empty.
+		boolean ownerReady = owner != null; // Must have owner.
+		boolean questionsReady = questions.size() >= 1; // Has atleast one question.
+		
+		return nameReady && ownerReady && questionsReady;
+	}
+	
+	/**
+	 * Creates the SurveyEntity, ready for storing in the database.
+	 * Make sure to call isReady() first.
+	 * @return The SurveyEntity associated with this model.
+	 */
+	public SurveyEntity createSurvey(){
+		
+		survey.setName(name);
+		survey.setOwner(owner);
+		survey.setActive(false);
+		survey.setQuestions(convertQuestionsToEntities());
+		
+		Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+		survey.setDateCreated(now);
+		survey.setCode(generateCode_TemporaryMethod());
+		
+		return survey;
+	}
+
+	/**
+	 * @deprecated Temporary
+	 * @return The code for the survey, which here will always be 'asdf'.
+	 */
+	private String generateCode_TemporaryMethod() {
+		return "asdf";
+	}
+	
+	/**
+	 * Converts the QuestionModel list into an QuestionEntity List.
+	 * @return a QuestionEntity List.
+	 */
+	private List<QuestionEntity> convertQuestionsToEntities(){
+		List<QuestionEntity> questionEntities = new ArrayList<>();
+		
+		for(SurveyQuestionCreationModel question : questions){
+			QuestionEntity questionEntity = question.getQuestionEntity(survey);
+			questionEntities.add(questionEntity);
+		}
+		
+		return questionEntities;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public UserEntity getOwner() {
+		return owner;
+	}
+
+	public List<SurveyQuestionCreationModel> getQuestions() {
+		return questions;
+	}
 	
 }
--- src/main/java/no/hib/megagruppe/webpoll/models/lecturer/SurveyQuestionCreationModel.java
+++ src/main/java/no/hib/megagruppe/webpoll/models/lecturer/SurveyQuestionCreationModel.java
@@ -1,5 +1,13 @@
 package no.hib.megagruppe.webpoll.models.lecturer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import no.hib.megagruppe.webpoll.entities.OptionEntity;
+import no.hib.megagruppe.webpoll.entities.QuestionEntity;
+import no.hib.megagruppe.webpoll.entities.QuestionEntity.QuestionType;
+import no.hib.megagruppe.webpoll.entities.SurveyEntity;
+
 public class SurveyQuestionCreationModel {
 
 	/*
@@ -12,5 +20,94 @@ public class SurveyQuestionCreationModel {
 	 * 
 	 * 
 	 */
+	
+	private QuestionType questionType;
+	private String questionText;
+	private String[] options;
+	
+	public SurveyQuestionCreationModel(QuestionType questionType, String questionText) {
+		this.questionType = questionType;
+		this.questionText = questionText;
+	}
+
+	
+	
+	
+	
+	
+	/**
+	 * Checks if the question is ready to be built.
+	 * If it is a multiple-choice question, it must have atleast one option.
+	 * If it is not a multiple-choice question then the options-array is empty.
+	 * @return True if there is atleast one option in the case of multiple-choice question.
+	 */
+	public boolean isReady(){
+		boolean hasOptionsIfMultipleChoice 
+				= questionType.isMultipleChoice() ? options.length >= 1 : options == null;
+		
+		return hasOptionsIfMultipleChoice;
+	}
+	
+	/**
+	 * Returns a QuestionEntity ready to be stored in the database.
+	 * Make sure to call isReady() before calling this method.
+	 * @param survey The survey that owns this question.
+	 * @return The QuestionEntity associated with this SurveyQuestionCreationModel.
+	 */
+	public QuestionEntity getQuestionEntity(SurveyEntity survey){
+		QuestionEntity questionEntity = new QuestionEntity();
+		questionEntity.setSurvey(survey);
+		questionEntity.setText(questionText);
+		questionEntity.setType(questionType);
+		questionEntity.setOptions(convertOptionsToEntities(questionEntity));
+		
+		return questionEntity;
+	}
+	
+	/**
+	 * Converts each option into a questionEntity.
+	 * It only converts it if the option-string is not empty or null.
+	 * @param questionEntity The question owning this option.
+	 * @return A list of OptionEntity objects.
+	 */
+	private List<OptionEntity> convertOptionsToEntities(QuestionEntity questionEntity){
+		List<OptionEntity> optionEntities = new ArrayList<>();
+		
+		for(String option : options){
+			boolean optionNotEmpty = option != null && option != "";
+			
+			if(optionNotEmpty){
+				OptionEntity optionEntity = new OptionEntity();
+				optionEntity.setText(option);
+				optionEntity.setQuestion(questionEntity);
+				optionEntities.add(optionEntity);
+			}
+		}
+		
+		return optionEntities;
+	}
+	
+	
+	
+
+	public QuestionType getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(QuestionType questionType) {
+		this.questionType = questionType;
+	}
+
+	public String getQuestionText() {
+		return questionText;
+	}
+
+	public void setQuestionText(String questionText) {
+		this.questionText = questionText;
+	}
+	
+	public void setOptions(String[] options){
+		this.options = options;
+	}
 	
 }
