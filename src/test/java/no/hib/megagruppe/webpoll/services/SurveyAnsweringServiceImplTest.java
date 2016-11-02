bf1a4ef0f8f3aa531b48de48d83d12e2d984280c
Erik <erikns@users.noreply.github.com>
2016-11-02 00:06:16 +0000

    Add FIXME comment in survey answering service test

--- src/test/java/no/hib/megagruppe/webpoll/services/SurveyAnsweringServiceImplTest.java
+++ src/test/java/no/hib/megagruppe/webpoll/services/SurveyAnsweringServiceImplTest.java
@@ -80,7 +80,7 @@ public class SurveyAnsweringServiceImplTest {
 		
 		ResponseEntity response = responseRepository.findById(1);
 		assertNotNull(response);
-		assertEquals(survey, response.getSurvey());
+		assertEquals(survey, response.getSurvey()); // FIXME: What if the repository returns a copy?
 		assertEquals(survey.getQuestions().get(0).getOptions().get(1), response.getAnswers().get(0).getOption());
 		assertEquals("Veldig bra", response.getAnswers().get(1).getFreetext());
 	}
