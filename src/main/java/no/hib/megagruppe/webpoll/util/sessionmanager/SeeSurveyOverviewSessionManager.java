768d7b7cadc761f9ef8d1c3066c1f59b445ae1f2
h148907 <magnusdk@users.noreply.github.com>
2016-11-05 17:10:41 +0000

    SurveyOverviewSessionManager initial commit

---
+++ src/main/java/no/hib/megagruppe/webpoll/util/sessionmanager/SeeSurveyOverviewSessionManager.java
@@ -0,0 +1,18 @@
+package no.hib.megagruppe.webpoll.util.sessionmanager;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SeeSurveyOverviewSessionManager extends SessionManager{
+
+	public SeeSurveyOverviewSessionManager(HttpServletRequest request) {
+		super(request);
+	}
+	
+	public Integer getID(){
+		return null;
+	}
+	
+	
+	
+
+}
