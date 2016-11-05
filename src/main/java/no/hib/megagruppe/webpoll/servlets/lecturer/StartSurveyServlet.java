ebc530836e874e1a9c24bfdcdd6a82942d9fa5a9
h145159 <h145159@9fc1927c-0711-df4b-8656-0a05da22bc1c>
2016-11-05 16:16:03 +0000

    Started implementing code on StartSurvey (not finished)

--- src/main/java/no/hib/megagruppe/webpoll/servlets/lecturer/StartSurveyServlet.java
+++ src/main/java/no/hib/megagruppe/webpoll/servlets/lecturer/StartSurveyServlet.java
@@ -1,32 +1,77 @@
 package no.hib.megagruppe.webpoll.servlets.lecturer;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import no.hib.megagruppe.webpoll.entities.SurveyEntity;
+import no.hib.megagruppe.webpoll.models.lecturer.SurveyCreationModel;
+import no.hib.megagruppe.webpoll.services.SecurityService;
+import no.hib.megagruppe.webpoll.services.SurveyOverviewService;
+import no.hib.megagruppe.webpoll.util.sessionmanager.CreateSurveySessionManager;
+
 /**
  * Servlet implementation class StartSurveyServlet
  * 
- * Skal starte en survey gjennom en Start Survey knapp på SurveyOverview siden.
+ * Skal starte en survey gjennom en Start Survey knapp på SurveyOverview siden. Når denne 
+ * kanppen trykkes, skal tiden som er satt inn bli sendt til en service som lagrer tiden,
+ * og sender tilbake til StartSurveyServlet.
  * 
  */
-@WebServlet("/StartSurveyServlet")
+@WebServlet("/startsurvey")
 public class StartSurveyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-   
+	@EJB
+    SecurityService securityService;
+	
+	SurveyOverviewService surveyoverview;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if(securityService.isLoggedIn()) {
+			CreateSurveySessionManager session = new CreateSurveySessionManager(request);
+			request.getRequestDispatcher("WEB-INF/createsurvey/changename.jsp").forward(request, response);
+			session.clearErrorMessages();
+			
+			/* Må endres til noe annet enn CreateSurveySessionManager */
+			
+		} else {
+			response.sendRedirect("index");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		if(securityService.isLoggedIn()) {
+			CreateSurveySessionManager session = new CreateSurveySessionManager(request);
+			
+			/* Må endres til noe annet enn CreateSurveySessionManager */
+			
+			Timestamp deadline = (Timestamp) request.getAttribute("deadline");
+			
+		/*	SurveyEntity survey = session.getSurveyEntity(); 
+			
+			Integer id = survey.getId();  
+			
+			surveyoverview.activateSurvey(deadline, id); */
+			
+			
+		} else {
+			response.sendRedirect("index");
+		}
 	}
 
 }
+	
+		/*CurrentSurvey = survey.getID();
+		CurrentSurvey.setTimestamp();*/
+				
+	
+
+
