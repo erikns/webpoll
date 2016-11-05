03466ef2b687169b03e0378623ac74c91116ba1a
h148907 <magnusdk@users.noreply.github.com>
2016-11-05 17:12:08 +0000

    Iplemented getId

--- src/main/java/no/hib/megagruppe/webpoll/util/sessionmanager/SeeSurveyOverviewSessionManager.java
+++ src/main/java/no/hib/megagruppe/webpoll/util/sessionmanager/SeeSurveyOverviewSessionManager.java
@@ -1,18 +1,20 @@
 package no.hib.megagruppe.webpoll.util.sessionmanager;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-public class SeeSurveyOverviewSessionManager extends SessionManager{
+public class SeeSurveyOverviewSessionManager extends SessionManager {
+
+	private final String SURVEY_ID = "surveyid";
 
 	public SeeSurveyOverviewSessionManager(HttpServletRequest request) {
 		super(request);
 	}
-	
-	public Integer getID(){
-		return null;
+
+	public Integer getID() {
+		HttpSession session = request.getSession();
+
+		return (Integer) session.getAttribute(SURVEY_ID);
 	}
-	
-	
-	
 
 }
