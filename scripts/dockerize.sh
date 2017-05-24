65c2aa9ef667a4f63ea4f5a12bbdad8817fc79a5
Erik Sørensen <eriksorensen88@gmail.com>
2017-05-24 19:30:37 +0200

    Write documentation comment in dockerize.sh and make generation of image-file optional with command line flag, because this is a fairly time consuming operation

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script creates a Docker image of the solution. Use the --imagefile flag
+# to also generate a .tar.gz image for transfer.
+
 set -e
 
 BUILD_TAG=${TRAVIS_BUILD_NUMBER:-DEV}
@@ -11,7 +14,9 @@ echo "Building docker image..."
 docker build -t $IMAGE_TAG .
 echo "OK"
 
-echo "Generating image artifact..."
-docker save $IMAGE_TAG | gzip -v > $IMAGE_FILE
-echo "OK"
+if [ "$1" = "--imagefile" ]; then
+    echo "Generating image artifact..."
+    docker save $IMAGE_TAG | gzip > $IMAGE_FILE
+    echo "OK"
+fi
 
