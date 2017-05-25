7793d4e21c8361580cf1a1275fb28604524d74b1
Erik Sørensen <eriksorensen88@gmail.com>
2017-05-25 21:55:25 +0200

    Make scp in deploy stop nagging and report error properly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
-
 echo "Deploying image..."
-scp -P $DEPLOY_PORT \
+
+scp -o StrictHostKeyChecking=no -o UserKnownHotsFile=/dev/null \
+    -q -P $DEPLOY_PORT \
     webpoll-$TRAVIS_BUILD_NUMBER.tar.gz \
     travis@$DEPLOY_HOST:~/webpoll-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.tar.gz
+if [ $? -ne 0 ]; then
+    echo "Deploy failed!"
+    exit 1
+fi
+
 echo "OK"
+exit 0
 
