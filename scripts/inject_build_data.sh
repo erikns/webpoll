81a4a828a465467ab9947d0175e9e0bb08c98b09
Erik Sørensen <eriksorensen88@gmail.com>
2017-05-27 02:23:28 +0200

    Add shorter commit sha as build data

--- scripts/inject_build_data.sh
+++ scripts/inject_build_data.sh
@@ -6,5 +6,6 @@ echo "Injecting build number into resource path..."
 echo ${TRAVIS_BUILD_NUMBER} > src/main/web/WEB-INF/buildNumber
 
 echo "Injecting revision number into resource path..."
-echo ${TRAVIS_COMMIT} > src/main/web/WEB-INF/revision
+COMMIT_SHA=${TRAVIS_COMMIT} 
+echo ${COMMIT_SHA:0:7} > src/main/web/WEB-INF/revision
 
