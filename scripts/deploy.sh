a1cc3a3f9b2e8de0e66e23a7630857f8939d3d94
Erik Sørensen <eriksorensen88@gmail.com>
2017-05-27 02:42:41 +0200

    Mark PR builds in Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,16 @@
 
 echo "Deploying image..."
 
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    PR=""
+else
+    PR="-PR$TRAVIS_PULL_REQUEST"
+fi
+
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
     -q -P $DEPLOY_PORT \
     webpoll-$TRAVIS_BUILD_NUMBER.tar.gz \
-    travis@$DEPLOY_HOST:~/webpoll-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.tar.gz
+    travis@$DEPLOY_HOST:~/webpoll-$TRAVIS_BRANCH$PR-$TRAVIS_BUILD_NUMBER.tar.gz
 if [ $? -ne 0 ]; then
     echo "Deploy failed!"
     exit 1
